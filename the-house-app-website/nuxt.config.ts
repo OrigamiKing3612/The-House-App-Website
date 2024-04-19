36ab202a2c602fabe8beaef2855c0416c5fb2820
OrigamiKing3612 <115757805+OrigamiKing3612@users.noreply.github.com>
2024-04-19 18:44:08 +0000

    More stuff

--- the-house-app-website/nuxt.config.ts
+++ the-house-app-website/nuxt.config.ts
@@ -1,4 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `@use "@/assets/styles/main.scss" as *;`
+        }
+      }
+    }
+  }
 })
---
+++ the-house-app-website/pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="all">
+        <h1> The House App</h1>
+    </div>
+</template>
+
+<style scoped lang="scss">
+.all {}
+</style>
