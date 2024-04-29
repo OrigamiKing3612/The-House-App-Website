7dbf5af0c4312022cccbab316dd27b8e31198078
OrigamiKing3612 <115757805+OrigamiKing3612@users.noreply.github.com>
2024-04-29 15:27:47 +0000

    Add metadata

--- the-house-app-website/nuxt.config.ts
+++ the-house-app-website/nuxt.config.ts
@@ -1,5 +1,28 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+const title = "The House App";
+const meta = {
+  description: "The House App",
+  publisher: "OrigamiKing3612",
+  "og:title": title,
+  "og:site_name": title,
+  "og:description": "The House App",
+  "og:type": title,
+  "og:url": "the-house-app.com"
+};
+
 export default defineNuxtConfig({
+  app: {
+    head: {
+      htmlAttrs: {
+        lang: "en"
+      },
+      title: title,
+      meta: Object.entries(meta).map(([name, content]): object => {
+        return { name, content };
+      })
+    },
+    viewTransition: true
+  },
   devtools: { enabled: true },
   vite: {
     css: {
