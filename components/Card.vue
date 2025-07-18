ee50ecbb5d22d1436c251cdf2525feb1d924698f
OrigamiKing3612 <115757805+OrigamiKing3612@users.noreply.github.com>
2025-07-18 13:21:44 -0500

    Update the styles

--- components/Card.vue
+++ components/Card.vue
@@ -1,42 +1,25 @@
 <template>
-  <div
-    class="card"
-    :style="{ width: props.width + 'vw', 'max-width': props.maxWidth + 'px', 'margin-bottom': props.marginBottom + 'px' }"
-  >
-    <div class="v-container">
-      <slot />
+    <div class="card">
+        <div class="v-container">
+            <slot />
+        </div>
     </div>
-  </div>
 </template>
 
 <script setup lang="ts">
-const props = defineProps({
-  marginBottom: {
-    type: String,
-    default: "10"
-  },
-  width: {
-    type: String,
-    default: "66"
-  },
-  maxWidth: {
-    type: String,
-    default: "450"
-  }
-});
 </script>
 
 
 <style lang="scss" scoped>
 .card {
-  padding: 20px;
-  background-color: var(--main-background);
-  border-radius: 20px;
-  border: 1px solid var(--text);
+    padding: 20px;
+    background-color: var(--main-background);
+    border-radius: var(--border-radius);
+    border: var(--border);
+    max-width: 450px;
 
-  .v-container {
-    flex-direction: column;
-  }
+    .v-container {
+        flex-direction: column;
+    }
 }
 </style>
-
