5a35c5beec1c66343a9d5fa29dc3fcd5984b80e7
OrigamiKing3612 <origamiking3612@gmail.com>
2025-07-22 09:45:04 -0500

    Add a bugs page

---
+++ app/pages/bugs.vue
@@ -0,0 +1,24 @@
+<template>
+    <CardWithTitle title="Bugs" class="title">
+
+    </CardWithTitle>
+    <RoundedCard>
+        <template #default>
+
+        </template>
+        <template #title>
+
+        </template>
+    </RoundedCard>
+</template>
+
+<style lang="scss" scoped>
+.title {
+    width: 85%;
+    max-width: 700px;
+}
+
+.left-aligned {
+    text-align: left;
+}
+</style>
