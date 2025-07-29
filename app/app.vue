1641ccb02f545707e0b8613cf9aba25fcbfcc262
OrigamiKing3612 <origamiking3612@gmail.com>
2025-07-28 22:47:53 -0500

    fix bug

--- app/app.vue
+++ app/app.vue
@@ -1,53 +1,56 @@
 <template>
-  <NuxtLayout>
-    <NuxtPage />
-  </NuxtLayout>
+    <NuxtLayout>
+        <NuxtPage />
+    </NuxtLayout>
 </template>
 
 <script setup lang="ts">
 </script>
 
 <style scoped lang="scss">
+@use "@/assets/styles/main.scss" as *;
+
 .window-controls {
-  display: flex;
-  flex-direction: row;
-  z-index: 999;
-  align-items: center;
-
-  .titlebar-button {
     display: flex;
+    flex-direction: row;
+    z-index: 999;
     align-items: center;
-    justify-content: center;
-    cursor: pointer;
-    transition: all ease-in-out 0.1s;
-    background-color: var(--main-background);
-    color: var(--text);
-    border-radius: 0;
-    border: 0;
 
-    &.close {
-      &:hover,
-      &:active {
-        background-color: red;
+    .titlebar-button {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        cursor: pointer;
+        transition: all ease-in-out 0.1s;
+        background-color: var(--main-background);
         color: var(--text);
-      }
+        border-radius: 0;
+        border: 0;
+
+        &.close {
+
+            &:hover,
+            &:active {
+                background-color: red;
+                color: var(--text);
+            }
+        }
+
+        &:hover,
+        &:active {
+            background-color: var(--background-primary);
+            color: var(--text);
+        }
     }
-
-    &:hover,
-    &:active {
-      background-color: var(--background-primary);
-      color: var(--text);
-    }
-  }
 }
 
-.list-enter-active, .list-leave-active {
-  transition: opacity 1.0s;
+.list-enter-active,
+.list-leave-active {
+    transition: opacity 1.0s;
 }
 
-.list-enter, .list-leave-to {
-  opacity: 0;
+.list-enter,
+.list-leave-to {
+    opacity: 0;
 }
-
 </style>
-
