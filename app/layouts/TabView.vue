6051164a526bfbf3a12ef62da173ec8561f7d228
OrigamiKing3612 <origamiking3612@gmail.com>
2025-08-10 22:40:45 -0500

    add a description

--- app/layouts/TabView.vue
+++ app/layouts/TabView.vue
@@ -10,6 +10,7 @@
 <script setup lang="ts">
 useSeoMeta({
     title: "The House App",
+    description: "The House App is a cutting-edge tool designed to enrich the educational journey for students and teachers alike. It offers insights to track house-related activities and achievements, fostering engagement within the school community.",
     ogTitle: "The House App",
     ogUrl: "https://the-house-app.com",
     ogImage: "https://the-house-app.com/web-app-manifest-192x192.png",
