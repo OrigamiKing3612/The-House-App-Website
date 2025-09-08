baa16b74129696891aeba89ac2c33710d59e1087
OrigamiKing3612 <origamiking3612@gmail.com>
2025-09-08 17:18:23 +0000

    initial commit

---
+++ app/pages/changelog/index.vue
@@ -0,0 +1,25 @@
+<template>
+    <Card title="Changelog">
+        <div v-for="v in data">
+        <NuxtLink :to="`/changelog/v/${v.text}`" class="link">
+        <AppSubmitButton>
+            {{ v.version }}
+        </AppSubmitButton>
+        </NuxtLink>
+        </div>
+    </Card>
+</template>
+
+<script lang="ts" setup>
+const data = [
+    {
+        text: "0.4.0",
+        version: "v0.4.0"
+    },
+]
+
+</script>
+
+<style scoped lang="scss">
+
+</style>
---
+++ app/pages/changelog/v/0.4.0.vue
@@ -0,0 +1,19 @@
+<template>
+<Card :title="title">
+
+</Card>
+</template>
+
+<script lang="ts" setup>
+
+const title = ref("v0.4.0 - [title]")
+
+definePageMeta({
+    layout: "default",
+    title: title.value
+});
+</script>
+
+<style lang="scss" scoped>
+
+</style>
