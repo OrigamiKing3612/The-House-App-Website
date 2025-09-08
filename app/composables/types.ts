bf87ffda45ae1d66f55a1add0ca8af444b30f225
OrigamiKing3612 <origamiking3612@gmail.com>
2025-09-08 09:17:29 -0500

    Auto imports (#27)

--- app/composables/types.ts
+++
@@ -1,5 +0,0 @@
-export type { GitHubIssue } from "./types/GitHubIssue";
-export type { GitHubLabel } from "./types/GitHubLabel";
-
-export { newGitHubIssue } from "./types/GitHubIssue";
-export { newGitHubLabel } from "./types/GitHubLabel";
