9d471fe6e2e0e5679987dd1a6576603021d6a0c3
OrigamiKing3612 <origamiking3612@gmail.com>
2025-09-22 20:32:13 -0500

    inital commit

---
+++ app/pages/changelog/v/0.4.1.vue
@@ -0,0 +1,10 @@
+<template>
+    <ChangelogPage :data="data.changelogs['0.4.1']">
+        <p>This update brings bug fixes</p>
+        <h1>Bug Fixes</h1>
+        <ul>
+            <li>The live updaing pages should now work on Safari</li>
+            <li>The User Types Allowed for presets can now be edited</li>
+        </ul>
+    </ChangelogPage>
+</template>
