c11fdfac7ede4538e976dff616eeb549f84d82ee
OrigamiKing3612 <origamiking3612@gmail.com>
2025-10-13 20:31:57 -0500

    copilot suggestions

--- app/pages/changelog/v/0.4.2.vue
+++ app/pages/changelog/v/0.4.2.vue
@@ -5,13 +5,13 @@
         <p>We have redesigned the Edit Users page to be in a table format. You can now see, sort, and take actions
             easier.</p>
         <h1>Authentication</h1>
-        <p>We have redone out authentication to be more scalable and secure. This will require <i>everyone</i> to log
+        <p>We have redone our authentication to be more scalable and secure. This will require <i>everyone</i> to log
             back in.</p>
         <h1>Elevate Popups</h1>
         <p>When deleting something, admins now have an elevate popup to prevent accidental deletions.</p>
         <h1>Bug Fixes</h1>
         <ul>
-            <li>Points in the points feed can now be clicked to see more infomation</li>
+            <li>Points in the points feed can now be clicked to see more information</li>
             <li>Fixed a bug where points looked like they are added twice in games</li>
         </ul>
     </ChangelogPage>
