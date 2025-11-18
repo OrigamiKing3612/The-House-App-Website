47126f233c78000d3bdf2b00f1e901caf03775c1
OrigamiKing3612 <origamiking3612@gmail.com>
2025-11-17 21:07:45 -0600

    update nuxt shared

--- app/pages/bugs/[id].vue
+++ app/pages/bugs/[id].vue
@@ -1,3 +1,14 @@
 <template>
 
 </template>
+
+<script setup lang="ts">
+const id = useRoute().params.id as string;
+
+const issue = ref<GitHubIssue | null>(null);
+const store = useStore();
+
+onMounted(async () => {
+    issue.value = store.issues.find
+});
+</script>
