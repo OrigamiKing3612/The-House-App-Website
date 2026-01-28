be338c8a4201002807a76a3855828bbae970ac96
OrigamiKing3612 <origamiking3612@gmail.com>
2026-01-27 21:13:45 -0600

    work on the titlebar and sidebar

---
+++ src/utils/SubmitContact.ts
@@ -0,0 +1,20 @@
+export const SubmitContact = async (name: string, email: string, organization: string, hear: string, question?: string) => {
+    try {
+        await fetch("https://n8n.origamiking3612.com/webhook/the-house-app/contact", {
+            method: "POST",
+            body: JSON.stringify({
+                name: name,
+                email: email,
+                organization: organization,
+                hear: hear,
+                question: question,
+                site: "the-house-app.com",
+            })
+        })
+
+        return true;
+    } catch (error: any) {
+        console.error("API error:", error.message)
+        return []
+    }
+}
