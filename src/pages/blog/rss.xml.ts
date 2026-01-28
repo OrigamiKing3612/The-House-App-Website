51ec0a62e6810e3e70dac7637b86468c97b0e9d2
OrigamiKing3612 <origamiking3612@gmail.com>
2026-01-27 22:24:52 -0600

    clean up code

--- src/pages/blog/rss.xml.ts
+++ src/pages/blog/rss.xml.ts
@@ -2,10 +2,6 @@ import type { APIRoute } from "astro";
 import { getCollection } from 'astro:content';
 import rss from '@astrojs/rss';
 
-function sortPosts(a: Date, b: Date) {
-    return Number(b) - Number(a);
-}
-
 function formatDate(date: Date) {
     date.setUTCHours(0);
     return date;
@@ -13,17 +9,19 @@ function formatDate(date: Date) {
 
 export const GET: APIRoute = async (context) => {
     const unsortedPosts = await getCollection('blog');
-    const posts = unsortedPosts.sort((a, b) => sortPosts(a.data.timestamp, b.data.timestamp));
+    const posts = unsortedPosts.sort(
+        (a, b) => Number(b.data.timestamp) - Number(a.data.timestamp),
+    );
 
     return rss({
         title: "The House App",
         description: "Updates and news from The House App",
         site: (context.site as URL).href,
-        items: posts.map((item) => ({
-            title: item.data.title,
-            description: item.data.description,
-            link: `/blog/${item.id}/`,
-            pubDate: formatDate(item.data.timestamp),
+        items: posts.map(({ data }) => ({
+            title: data.title,
+            description: data.description,
+            link: `/blog/${data.slug}/`,
+            pubDate: formatDate(data.timestamp),
         })),
     })
 }
